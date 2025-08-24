4b9ef5cdce2a253d33002760e09dc6ff08eb270f
Daniil <daniil.tkachenko.05@mail.ru>
2025-08-24 14:39:27 +0300

    'create mocks'

---
+++ src/features/boards-list/boards-list.page.tsx
@@ -0,0 +1,19 @@
+import { rqClient } from "@/shared/api/instance";
+import { CONFIG } from "@/shared/model/config";
+import { ROUTES } from "@/shared/model/routes";
+import { href, Link } from "react-router-dom";
+
+function BoardsListPage() {
+  const boardsQuery = rqClient.useQuery("get", "/boards");
+
+  return (
+    <div>
+      <h1>Boards List {CONFIG.API_BASE_URL}</h1>
+      {boardsQuery.data?.map((board) => (
+        <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>
+      ))}
+    </div>
+  );
+}
+
+export const Component = BoardsListPage;
